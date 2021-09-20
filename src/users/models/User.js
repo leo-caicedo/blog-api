58712b1375c146aa54a49eb30af1d7e379c415c5
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-09-20 14:23:58 -0500

    User model

---
+++ src/users/models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
